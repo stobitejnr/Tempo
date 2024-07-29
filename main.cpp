dc6dbe47f7d2d378366413b8cd088c4fdef6eba8
Rowan Zeszut <rowanzeszut@gmail.com>
2024-07-29 22:35:40 +1200

    cleanup main, regex detection

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <conio.h>
 #include <vector>
+#include <regex>
 
 #include "Timer.hpp"
 #include "Display.hpp"
@@ -21,24 +22,40 @@ Timer createTimer(){
 
     Display::clearScreen();
 
-    cout << "How long would you like to set a timer for?" << endl;
-
     while(true){
-    cout << "Hours: "; cin >> h;
-    if(h <= 99 && h>= 0) {break;}
-    cout<<"Invalid number of hours."<<endl;}
+        cout << "How long would you like to set a timer for?" << endl;
+        string input = "";
+        getline(cin,input);
 
-    while(true){
-    cout << "Minutes: "; cin >> m;
-    if(m <= 59 && m>= 0) {break;}
-    cout<<"Invalid number of minutes."<<endl;}
+    
+        //Regular expressions :D
+        regex hours_regex(R"((\d+)\s*hours?)");
+        regex minutes_regex(R"((\d+)\s*minutes?)");
+        regex seconds_regex(R"((\d+)\s*seconds?)");
 
-    while(true){
-    cout << "Seconds: "; cin >> s;
-    if(s <= 59 && s>= 0) {break;}
-    cout<<"Invalid number of seconds."<<endl;}
+        smatch match;
 
-    countdownSeconds = (3600*h) + (60*m) + (s);
+        //Find hours
+        if (regex_search(input, match, hours_regex)) {
+        h = stoi(match[1].str());
+        }
+
+        // Find minutes
+        if (regex_search(input, match, minutes_regex)) {
+            m = stoi(match[1].str());
+        }
+
+        // Find seconds
+        if (regex_search(input, match, seconds_regex)) {
+            s = stoi(match[1].str());
+        }
+        
+        countdownSeconds = (3600*h) + (60*m) + (s);
+        if (countdownSeconds <= 360000){break;}
+        else{
+            cout << "Cannot set a timer for more than 100 hours." << endl;
+        }
+    }
 
     Timer timer = Timer(1, countdownSeconds, "Name", "Description");
 
@@ -48,6 +65,9 @@ Timer createTimer(){
 
 // Main function
 int main() {
+
+    vector<Timer> timers;
+    vector<Display> displays;
     
     Timer timer = createTimer();
     Display display = Display(timer);
@@ -73,6 +93,6 @@ int main() {
     }
 
     
-    cout << "\nCountdown finished!" << endl;
+    cout << "\nTimer finished!" << endl;
     return 0;
 }
