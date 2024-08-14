5fa2a9629aa9e72325abf2c79bcef10d76ff0858
Rowan Zeszut <rowanzeszut@gmail.com>
2024-08-15 10:59:58 +1200

    fonts modular

---
+++ fonts/font1.hpp
@@ -0,0 +1,12 @@
+#ifndef FONT1_HPP
+#define FONT1_HPP
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+extern vector<vector<string>> font1;
+
+#endif // FONT1_HPP
