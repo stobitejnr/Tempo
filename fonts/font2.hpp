fb462e3c140c003e9c02fe3f63c5f792df2b0750
William <donwi381@student.otago.ac.nz>
2024-08-27 09:31:18 +1200

    created new font
    isnt actually able to be used in the code

---
+++ fonts/font2.hpp
@@ -0,0 +1,13 @@
+#ifndef FONT2_HPP
+#define FONT2_HPP
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+using namespace std;
+
+extern vector<vector<string>> font2;
+
+#endif // FONT2_HPP
