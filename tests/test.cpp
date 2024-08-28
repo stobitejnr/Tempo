cad25ca98bb815c946cd747fd16015651cc6b1e9
Rowan Zeszut <rowanzeszut@gmail.com>
2024-08-29 09:14:01 +1200

    test functions

---
+++ tests/test.cpp
@@ -0,0 +1,34 @@
+#include "../include/Menu.hpp"
+#include "../include/Display.hpp"
+
+#include <assert.h>
+
+
+
+void testTimer(){
+    
+}
+
+void testStopwatch(){
+    
+}
+
+void testAlarm(){
+    
+}
+
+void testMenu(){
+    
+}
+
+void testDisplay(){
+    
+}
+
+int main(){
+
+    testTimer();
+
+
+    return 0;
+}
