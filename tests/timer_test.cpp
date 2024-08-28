3384258471114bfeb9c624e5a2fd8ff329f8b30c
William <donwi381@student.otago.ac.nz>
2024-08-29 10:40:04 +1200

    Finish timer test.cpp

--- tests/timer_test.cpp
+++ tests/timer_test.cpp
@@ -1,9 +1,8 @@
 #include <cassert>
-
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include "include/Timer.hpp"
+#include "../include/Timer.hpp"
 
 using namespace std;
 
@@ -29,21 +28,58 @@ void test_pause_resume(){
     assert(timer.remainingMilliseconds() < 59000);
 }
 
+/*
+*   Tests that the reset reverts back to original time and is stopped
+*/
+void test_reset(){
+    Timer timer(0,1,0);
+    this_thread::sleep_for(chrono::seconds(1));
+    timer.reset();
+    assert(timer.remainingMilliseconds() == 60000);
+    assert(!timer.isRunning());
+}
 
+/*
+*   Tests adding time increases remaining time
+*/
+void test_add_time(){
+    Timer timer(0,1,0);
+    timer.addTime(30);
+    assert(timer.remainingMilliseconds() == 90000);
+}
 
+/*
+*   Test percent elapsed for the progress bar
+*/
+void test_percent_elapsed(){
+    Timer timer(0,1,0);
+    this_thread::sleep_for(chrono::seconds(30));
+    double percent = timer.percentElapsed();
+    assert(percent > 49.0 && percent < 51.0); // allows for a bit or margin of error
+}
 
+/*
+*   Testing the timer stops when it finishes 
+*/
+void test_expiration() {
+    Timer timer(0, 0, 2); // 2 seconds
+    this_thread::sleep_for(chrono::seconds(3));
+    assert(timer.remainingMilliseconds() == 0);
+    assert(!timer.isRunning());
+}
 
-
-
-int main(){
+int main() {
     try {
         test_constructor();
         test_pause_resume();
-        cout <<"all tests passed";
-        } catch (const std::exception& e) {
+        test_reset();
+        test_add_time();
+        test_percent_elapsed();
+        test_expiration();
+        std::cout << "All tests passed successfully!\n";
+    } catch (const exception& e) {
         std::cerr << "Test failed with exception: " << e.what() << std::endl;
         return 1;
     }
     return 0;
 }
-
