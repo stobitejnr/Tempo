aaeb5dcca89050df992e849283ae3187cd38cad6
Kevin Albert <albke154@student.otago.ac.nz>
2024-08-29 12:58:51 +1200

    cleaning

--- docs/html/search/functions_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['remainingmilliseconds_0',['remainingMilliseconds',['../class_timer.html#a330607659937f497389c9989812cd925',1,'Timer']]],
-  ['reset_1',['reset',['../class_stopwatch.html#a42c7014e7fffcf4c56ca6fb07f8eb31c',1,'Stopwatch::reset()'],['../class_timer.html#a9020542d73357a4eef512eefaf57524b',1,'Timer::reset()']]],
-  ['resume_2',['resume',['../class_stopwatch.html#a789461171238250633eeb26b8f3b0dce',1,'Stopwatch::resume()'],['../class_timer.html#a4ac55a73bb3431db9d4d2fd70ae9a2e8',1,'Timer::resume()']]]
-];
