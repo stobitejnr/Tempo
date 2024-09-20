5bcd766fbcbc34f100c11c5db26136a6eea6308a
William <donwi381@student.otago.ac.nz>
2024-09-20 12:17:48 +1200

    Created Two new fonts havent been tested

---
+++ fonts/font4.hpp
@@ -0,0 +1,12 @@
+#ifndef FONT4_HPP
+#define FONT4_HPP
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+extern vector<vector<string>> font4;
+
+#endif // FONT4_HPP
